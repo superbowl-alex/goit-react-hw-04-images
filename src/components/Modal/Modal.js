dd987d049f1550d8165c780b7bd7f0f94712df58
Oleksandr <superbowl@ukr.net>
2022-10-13 21:29:48 +0300

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { Overlay, ModalWindow } from './Modal.styled';
@@ -6,21 +6,18 @@ import { Overlay, ModalWindow } from './Modal.styled';
 const modalRoot = document.querySelector('#modal-root');
 
 function Modal({ image, toggleModal }) {
-  const handleKeyDown = useCallback(
-    e => {
+  useEffect(() => {
+    const handleKeyDown = e => {
       if (e.code === 'Escape') {
         toggleModal();
       }
-    },
-    [toggleModal]
-  );
+    };
 
-  useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [handleKeyDown]);
+  }, [toggleModal]);
 
   const handleBackDropClick = e => {
     if (e.currentTarget === e.target) {
